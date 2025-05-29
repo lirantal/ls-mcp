4d07b5fbb454f00b7371c4dd2708e086064e79a9
Liran Tal <liran.tal@gmail.com>
2025-05-29 20:58:03 +0200

    chore: fix up linting style
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -3,22 +3,19 @@ import assert from 'node:assert'
 import { MCPFiles } from '../src/main.ts'
 
 describe('CLI program', () => {
-
   beforeEach(() => {
     // Reset the mocks before each test
     mock.reset()
-  });
+  })
 
   test('find MCP files', async (t) => {
-
     const mockedMCPFileFixtures = [
       '__tests__/__fixtures__/mcp.json',
       '__tests__/__fixtures__/subdir/mcp2.json',
-    ];
+    ]
 
-    const mcpFilesManager = new MCPFiles(mockedMCPFileFixtures);
-    const mcpFilesList = await mcpFilesManager.findFiles();
-    assert.deepStrictEqual(mcpFilesList, mockedMCPFileFixtures);
+    const mcpFilesManager = new MCPFiles(mockedMCPFileFixtures)
+    const mcpFilesList = await mcpFilesManager.findFiles()
+    assert.deepStrictEqual(mcpFilesList, mockedMCPFileFixtures)
   })
-
-});
+})
